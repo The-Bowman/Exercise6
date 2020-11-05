35c98a151c9ce234cb314b4704dc0ee8edd4f4d1
The-Bowman <72958677+The-Bowman@users.noreply.github.com>
2020-11-04 22:40:48 -0700

    Add project files.

---
+++ Exercise6/Dice.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise6
+{
+    class Dice
+    {
+        private int numSides;   //number of sides on the dice, ranging from 4 to 20
+        private int sideRolled; //the side facing up on the dice
+
+        public Dice(int i)
+        {
+            setSides(i);
+        }
+
+        //sets the number of sides of a dice between 4 and 20
+        public int setSides(int i)
+        {
+            if (i < 4 )
+            {
+                return numSides= 4;
+            } else if (i > 20)
+            {
+                return numSides = 20;
+            } else
+            {
+                return numSides = i;
+            }
+        }
+
+        public int roll()
+        {
+            Random rand = new Random();
+            return sideRolled =  rand.Next(1, this.getNumSides() + 1);
+           
+        }
+
+        public int getNumSides()
+        {
+            return this.numSides;
+        }
+
+        public int getRoll()
+        {
+            return this.sideRolled;
+        }
+    }
+}
---
+++ Exercise6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise6
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            Dice dice1 = new Dice(1);
+            Dice dice2 = new Dice(10000);
+
+            int rollCounter = 0;
+
+            Console.WriteLine("Initial: " + dice1.getNumSides());
+            Console.WriteLine("Initial dice2: " + dice2.getNumSides());
+
+            dice1.setSides(20);
+            Console.WriteLine("After assignment: " + dice1.getNumSides());
+
+            do
+            {
+                dice1.roll();
+                dice2.roll();
+                rollCounter++;
+                Console.WriteLine("Roll: " + dice1.getRoll() + " " + dice2.getRoll());
+            } while (dice1.getRoll() != 1 || dice2.getRoll() != 1);
+
+            Console.WriteLine("SNAKE EYES: " + dice1.getRoll() + " : " + dice2.getRoll());
+            Console.WriteLine("Number of rolls to get snake eyes: " + rollCounter);
+            
+        }
+    }
+}
